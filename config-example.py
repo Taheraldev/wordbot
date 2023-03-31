5cca5a8ccb905078a687faf7225a8caea5e437d3
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2023-03-31 15:44:50 +0300

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -1,4 +1,4 @@
-bot_token = 'XXXX-XXXX'
-start_message = ''' Enter some message'''
+bot_token = '5120587277:AAHzmleJj8uWbsupjtlwlCjCsqcBAHI-sKI'
+start_message = '''hello bot English'''
 wordnik_api = 'http://api.wordnik.com:80/v4/word.json/'
-wordnik_api_key = 'your wordnik api key'
+wordnik_api_key = 'yraln8b3u6tlfeloi86na8plajchfat8x6wkzpzzjd00rhejj'
